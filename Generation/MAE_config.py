6ef7510599e0976df92e59339218064c8b858c73
980135330 <980135330@qq.com>
2022-07-13 22:51:52 +0800

    patch 8

--- Generation/MAE_config.py
+++ Generation/MAE_config.py
@@ -4,13 +4,13 @@ work_dir = "./"
 use_wandb = False
 
 
-batch_size = 128
+batch_size = 64
 epoch = 10
 mode = "train"
 # MAE transformer 设置
 img_size = 224
-embed_dim = 768
-patch_size = 16
+embed_dim = 1024
+patch_size = 8
 num_heads=16
 dim_head=embed_dim//num_heads
 depth=24
